73e93256cc6b28223d6819ce16e2b553b209bcbf
KhairulAmirinKA <u2101603@siswa.um.edu.my>
2023-07-19 17:31:49 +0800

    19/7.

---
+++ app/src/main/java/com/example/freecodecampeg/LearnUI.java
@@ -0,0 +1,13 @@
+package com.example.freecodecampeg;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LearnUI extends AppCompatActivity {
+    @Override  //starting point
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.learn_ui); //link dgn xml
+    }
+}
