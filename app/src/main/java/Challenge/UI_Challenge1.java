ebbf415fafdf04030cbebd117547c4e81bf15d57
KhairulAmirinKA <u2101603@siswa.um.edu.my>
2023-08-02 17:30:56 +0800

    2/8. learns simple registration UI. UI_Challenge1.java ui_challenge1.xml strings.xml (store countries list for spinner)

---
+++ app/src/main/java/Challenge/UI_Challenge1.java
@@ -0,0 +1,16 @@
+package Challenge;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.freecodecampeg.R;
+
+public class UI_Challenge1 extends AppCompatActivity {
+    @Override  //starting point
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.ui_challenge1); //link dgn xml
+
+    }
+}
--- app/src/main/java/com/example/freecodecampeg/Learn_Font.java
+++ app/src/main/java/com/example/freecodecampeg/Learn_Font.java
@@ -1,15 +1,40 @@
 package com.example.freecodecampeg;
 
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.res.ResourcesCompat;
 
 public class Learn_Font extends AppCompatActivity {
+
+    private Button btnChangeFont;
+    private TextView txtHello;
 
     @Override  //starting point
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.learn_font); //link dgn xml
+
+        txtHello= findViewById(R.id.txtHello4);
+        btnChangeFont= findViewById(R.id.btnChangeFont);
+
+        //get font
+        Typeface typeface= ResourcesCompat.getFont(this, R.font.bauh);
+
+        btnChangeFont.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                txtHello.setTypeface(typeface); //change font
+            }
+        });
+
+
+
+
 
 
 
